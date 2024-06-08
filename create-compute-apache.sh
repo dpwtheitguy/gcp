b9b24450999c5ba331e387022cec591e3433523d
dpwtheitguy <daniel.p.wilson@live.com>
2024-06-07 20:44:27 -0700

    Create create-compute-apache.sh
    Create a quick Apache instances for testing

---
+++ create-compute-apache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create a quick web server
+
+
+  gcloud compute instances create www1 \
+    --zone=us-east4-b \
+    --tags=network-lb-tag \
+    --machine-type=e2-small \
+    --image-family=debian-11 \
+    --image-project=debian-cloud \
+    --metadata=startup-script='#!/bin/bash
+      apt-get update
+      apt-get install apache2 -y
+      service apache2 restart
+      echo "
+<h3>Web Server: www1</h3>" | tee /var/www/html/index.html'
